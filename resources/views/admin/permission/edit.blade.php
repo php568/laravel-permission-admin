06c34eba712268bb28145bb13728944e61168bc0
lilong <lilong@dgg.net>
2018-05-08 16:45:58 +0800

    修复权限编辑时uniqued问题

--- resources/views/admin/permission/edit.blade.php
+++ resources/views/admin/permission/edit.blade.php
@@ -4,6 +4,7 @@
     <div class="layui-elem-quote">更新权限</div>
     <form class="layui-form" action="{{route('admin.permission.update',['permission'=>$permission])}}" method="post">
         {{method_field('put')}}
+        <input type="hidden" name="id" value="{{ $permission->id }}">
         @include('admin.permission._from')
     </form>
 @endsection
