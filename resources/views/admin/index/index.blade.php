6a67db4d3a4862f70389189fd917f3f329095d22
lilong <lilong@dgg.net>
2018-06-15 14:15:14 +0800

    '准备更换后台所有UI界面'

--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -2,7 +2,30 @@
 
 @section('content')
     <div class="layui-elem-quote">后台主页内容</div>
-    <div class="layui-fluid">
-
+    <div class="layui-row layui-col-space30">
+        <div class="layui-col-lg3">
+            <div class="pannel layui-bg-blue">
+                <p>今日订单数</p>
+                <p id="order_count">0</p>
+            </div>
+        </div>
+        <div class="layui-col-lg3">
+            <div class="pannel layui-bg-green">
+                <p>今日销售总额(万元)</p>
+                <p id="total_day">0</p>
+            </div>
+        </div>
+        <div class="layui-col-lg3">
+            <div class="pannel layui-bg-gray">
+                <p>当月销售总额(万元)</p>
+                <p id="total_month">0</p>
+            </div>
+        </div>
+        <div class="layui-col-lg3">
+            <div class="pannel layui-bg-cyan">
+                <p>用户总数</p>
+                <p id="member_count">0</p>
+            </div>
+        </div>
     </div>
 @endsection
